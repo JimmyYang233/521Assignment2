f9c4deb2da64cd7e4d942fd4e39684b5ac8c0beb
Dan Ning <ydn2012@hotmail.com>
2018-10-18 00:45:41 -0400

    Cannon Finish

---
+++ 521Assignment2/Assets/Scripts/Cannon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class Cannon : MonoBehaviour
+{
+
+	public GameObject cannonBall;
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		facingMove();
+		shoot();
+	}
+
+	void facingMove()
+	{
+		Vector3 mousePosition = Input.mousePosition;
+		mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+		
+		Vector2 direction = new Vector2(mousePosition.x-transform.position.x, mousePosition.y - transform.position.y);
+		//Debug.Log(direction.x + ", " + direction.y);
+
+		if (direction.x <= 0f && direction.y >= 0f)
+		{
+			transform.right = -direction;
+		}
+	}
+
+	void shoot()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			GameObject newCannonBall = GameObject.Instantiate(cannonBall);
+			newCannonBall.GetComponent<Rigidbody2D>().velocity = -transform.right * speed;
+		}
+	}
+}
---
+++ 521Assignment2/Assets/Scripts/CannonBall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour
+{
+	private Rigidbody2D rb2d;
+	private float groundLevel = -1.2f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+		//Debug.Log(transform.position.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y<=groundLevel)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	private void FixedUpdate()
+	{
+	}
+}
